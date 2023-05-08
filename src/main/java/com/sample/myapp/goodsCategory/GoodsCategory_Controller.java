cc2261b8ce389cfc309bf4417323dbbda753d116
hyein <ohio0510@naver.com>
2023-05-08 13:17:01 +0900

    5월8일 상세페이지 디테일 작업 + 위, 아래, 상담원 고정버튼 생성(component/up_down_support.jsp)
    혜인

--- src/main/java/com/sample/myapp/goodsCategory/GoodsCategory_Controller.java
+++ src/main/java/com/sample/myapp/goodsCategory/GoodsCategory_Controller.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.sample.myapp.PageVO;
 import com.sample.myapp.goods.GoodsDAO;
+import com.sample.myapp.goods.GoodsSmallType;
 import com.sample.myapp.goods.GoodsStep1;
 import com.sample.myapp.goods.GoodsTypeVo;
 import com.sample.myapp.goods.GoodsVo;
@@ -27,6 +28,7 @@ public class GoodsCategory_Controller {
 	GoodsDAO goodsDAO;
 	@Autowired
 	LoveDAO loveDAO;
+
 	/*메 페이지 이동*/
 	@RequestMapping("/")
 	public String home() {
@@ -49,7 +51,7 @@ public class GoodsCategory_Controller {
 		map.put("orderBy", orderBy);
 	
 		List<LoveVO> loveList = new ArrayList<>();
-		List<GoodsVo> goodsList = goodsDAO.selectOrderBy(map);
+		List<GoodsStep1> goodsList = goodsDAO.selectOrderBy(map);
 		List<String> flag = new ArrayList<String>();
 		
 		if(userid.equals("-1")) {
@@ -58,10 +60,10 @@ public class GoodsCategory_Controller {
 			loveList = loveDAO.selectLoveUser(userid);
 		}
 		int a = goodsList.size();
-		for(int b=0; b<a; b++) {
-		//	System.out.println(goodsList.get(b).getGoodsId());
-		}
-		for(int i=0; i<goodsList.size(); i++) {
+		/*for(int b=0; b<a; b++) {
+			System.out.println(">D>D>D>>DD"+goodsList.get(b).toString());
+		}*/
+		/*for(int i=0; i<goodsList.size(); i++) {
 			for(int j=0; j<loveList.size(); j++) {
 			if(goodsList.get(i).getGoodsId()==loveList.get(j).getGoods_id()) {
 				flag.add("true");
@@ -70,12 +72,11 @@ public class GoodsCategory_Controller {
 			}
 			if(flag.size()==i)
 			flag.add("false");
-		}
-		
+		}*/
 		
 
-		
 		ArrayList<HashMap<String, Object>> mainImage=new ArrayList<HashMap<String,Object>>();
+		GoodsStep1 goods = null;
 		for(int i=0; i<a; i++) {
 			Map<String,Object> dbImage=new HashMap<String, Object>();
 			
@@ -84,18 +85,24 @@ public class GoodsCategory_Controller {
 			int price = 0;
 			int goodsid = 0;
 			
-			GoodsStep1 goods = goodsDAO.selectGoodsIndex(goodsList.get(i).getGoodsIndexId());
-			if(goods.getDbImages() == null || goods.getDbImages().isEmpty()) {
+			
+			//System.out.print(goodsList.get(i).getGoodsIndexId());
+			
+			goods = goodsDAO.selectGoodsIndex(goodsList.get(i).getGoodsIndexId());
+			//System.out.print(":D>DDD:D "+goods.toString());
+			String[] arr=(((goods.getDbImages().replace("[","")).replace("]","")).split(","));
+			if(arr[0] == null || arr[0].isEmpty() || arr[0].equals("null")) {
 				continue;
 			}else {
-				String[] arr=(((goods.getDbImages().replace("[","")).replace("]","")).split(","));
+				
 				for(int j=0; j<arr.length; j++) {
 					imageList.add(arr[j]);
 					name= goods.getGoodsName();
 					price=goods.getGoodsPrice();
-					goodsid = goodsList.get(i).getGoodsId();
+					goodsid = goodsList.get(i).getGoodsIndexId();
 				}
 			}
+			
 			dbImage.put("ImageList", imageList);
 			dbImage.put("goodsName", name);
 			dbImage.put("goodsPrice", price );
@@ -108,100 +115,24 @@ public class GoodsCategory_Controller {
 			System.out.println(mainImage.get(i).get("goodsName"));
 		}
 		System.out.println(mainImage.toString());
+		
+		
+		List<GoodsSmallType> goodsSmallType = goodsDAO.selectSmallType(goods.getGoodsSmallType());
+		
 		model.addAttribute("mainImage", mainImage);
  		model.addAttribute("goodsList",goodsList);
  		model.addAttribute("page",page);
  		model.addAttribute("count",count);
  		model.addAttribute("mainCategory", 1);
  		model.addAttribute("orderBy", orderBy);
- 		model.addAttribute("goodsType", goodsType);
+ 		model.addAttribute("goodsType", goods.getGoodsType());
  		model.addAttribute("flag",flag);
+ 		model.addAttribute("goodsSmallType",goodsSmallType);
+
  		return "user/goodsCategory";
 
 	}
 	
-	@RequestMapping("/goodsList_byhi")
- 	public String goods_orderBy(PageVO page,Model model,@RequestParam(required = false, defaultValue = "-1")String userid, @RequestParam(required = false, defaultValue = "0")Integer pageNum, @RequestParam(required = false, defaultValue = "-1")Integer goodsType, @RequestParam(required = false, defaultValue = "goods_date desc") String orderBy){
-		Map<String, Object> map = new HashMap<>();
-		map.put("goodsType", goodsType);
-		map.put("searchKeyword", null);
-		int count = goodsDAO.totalCount(map);
-		page.setPageList(count);
-		page.setPage(pageNum);
-		map.put("start",page.getStart());
-		map.put("size", page.getSize());
-		map.put("orderBy", orderBy);
-	
-		List<LoveVO> loveList = new ArrayList<>();
-		List<GoodsVo> goodsList = goodsDAO.selectOrderBy(map);
-		List<String> flag = new ArrayList<String>();
-		
-		if(userid.equals("-1")) {
-			loveList = loveDAO.selectLoveUser("1");
-		}else {
-			loveList = loveDAO.selectLoveUser(userid);
-		}
-		int a = goodsList.size();
-		for(int b=0; b<a; b++) {
-		//	System.out.println(goodsList.get(b).getGoodsId());
-		}
-		for(int i=0; i<goodsList.size(); i++) {
-			for(int j=0; j<loveList.size(); j++) {
-			if(goodsList.get(i).getGoodsId()==loveList.get(j).getGoods_id()) {
-				flag.add("true");
-				break;
-			}
-			}
-			if(flag.size()==i)
-			flag.add("false");
-		}
-		
-		
-
-		
-		ArrayList<HashMap<String, Object>> mainImage=new ArrayList<HashMap<String,Object>>();
-		for(int i=0; i<a; i++) {
-			Map<String,Object> dbImage=new HashMap<String, Object>();
-			
-			List<String> imageList = new ArrayList<String>();
-			String name = null;
-			int price = 0;
-			int goodsid = 0;
-			
-			GoodsStep1 goods = goodsDAO.selectGoodsIndex(goodsList.get(i).getGoodsIndexId());
-			if(goods.getDbImages() == null || goods.getDbImages().isEmpty()) {
-				continue;
-			}else {
-				String[] arr=(((goods.getDbImages().replace("[","")).replace("]","")).split(","));
-				for(int j=0; j<arr.length; j++) {
-					imageList.add(arr[j]);
-					name= goods.getGoodsName();
-					price=goods.getGoodsPrice();
-					goodsid = goodsList.get(i).getGoodsId();
-				}
-			}
-			dbImage.put("ImageList", imageList);
-			dbImage.put("goodsName", name);
-			dbImage.put("goodsPrice", price );
-			dbImage.put("goodsId", goodsid );
-			
-			mainImage.add((HashMap<String, Object>) dbImage);
-			
-		}
-		for(int i=0; i<mainImage.size(); i++) {
-			System.out.println(mainImage.get(i).get("goodsName"));
-		}
-		System.out.println(loveList.toString());
-		model.addAttribute("mainImage", mainImage);
- 		model.addAttribute("goodsList",goodsList);
- 		model.addAttribute("page",page);
- 		model.addAttribute("count",count);
- 		model.addAttribute("mainCategory", 1);
- 		model.addAttribute("orderBy", orderBy);
- 		model.addAttribute("goodsType", goodsType);
- 		model.addAttribute("flag",flag);
- 		return "user/goodsCategory_byhi";
- 	}
 	
 	//상품상세페이지
 	@RequestMapping("/detail")
