b95fce57a95617b8e917c1c45be827b3c869079e
hong-yujin <hhhyj1077@naver.com>
2023-05-25 11:29:50 +0900

    찜목록 마이페이지 - 홍

--- src/main/java/com/sample/myapp/love/LoveVO.java
+++ src/main/java/com/sample/myapp/love/LoveVO.java
@@ -2,6 +2,9 @@ package com.sample.myapp.love;
 
 import java.sql.Date;
 
+import com.sample.myapp.goods.GoodsStep1;
+import com.sample.myapp.goods.GoodsVo;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
@@ -14,6 +17,8 @@ public class LoveVO {
 	private String user_id;
 	private Integer goods_id;
 	private Integer goods_index_id;
+	private GoodsVo goods;
+	private GoodsStep1 index;
 	public LoveVO(int love_id,String user_id, int goods_id) {
 		this.love_id = love_id;
 		this.user_id = user_id;
